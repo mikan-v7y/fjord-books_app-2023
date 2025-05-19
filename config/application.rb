26450b044769cf653b888f3599c38695a85e6b45
mikan-v7y <mikan.v7y@gmail.com>
2025-05-19 18:21:12 +0900

    feat: デフォルト言語を日本語に設定

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ module BooksApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # デフォルト言語を設定
+    config.i18n.default_locale = :ja
+
     # I18nライブラリに訳文の探索場所を指示する
     I18n.load_path += Dir[Rails.root.join("config", "locale", "*.{yml}")]
 
