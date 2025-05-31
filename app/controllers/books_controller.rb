b178b012d939916f3a930230080808e802530f40
mikan-v7y <mikan.v7y@gmail.com>
2025-05-31 23:48:08 +0900

    refactor: 翻訳のキー名を変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -52,7 +52,7 @@ class BooksController < ApplicationController
     @book.destroy
 
     respond_to do |format|
-      format.html { redirect_to books_url, notice: t('books.index.notice.destory') }
+      format.html { redirect_to books_url, notice: t('books.index.notice.destroy') }
       format.json { head :no_content }
     end
   end
