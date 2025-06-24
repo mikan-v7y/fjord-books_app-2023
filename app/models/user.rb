7947693ebf5d5bee4cc4d845cddfe4906237ad07
mikan-v7y <mikan.v7y@gmail.com>
2025-06-24 16:17:39 +0900

    feat: アップロード可能なファイルを限定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  ACCEPTED_CONTENT_TYPES = ['image/jpeg', 'image/png', 'image/gif'].freeze
+
   has_one_attached :avatar
+
+  validates :avatar, attached: true, content_type: ACCEPTED_CONTENT_TYPES
 end
