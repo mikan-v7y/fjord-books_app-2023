9e6577729a497a3bc676ed8d321969ad48978987
mikan-v7y <mikan.v7y@gmail.com>
2025-06-27 20:47:36 +0900

    feat: 登録情報を変更できるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    added_attrs = %i[email password password_confirmation postal_code address profile]
+    added_attrs = %i[email password password_confirmation postal_code address self_introduction]
     devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
     devise_parameter_sanitizer.permit(:account_update, keys: added_attrs)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   get 'users/index'
   get 'users/show'
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users, only: [:index, :show]
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
