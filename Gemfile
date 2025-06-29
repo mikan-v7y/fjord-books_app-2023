4477ec87e081c574c9689af52a2460abe2f666e2
mikan-v7y <mikan.v7y@gmail.com>
2025-06-30 01:52:41 +0900

    feat: devise画面を多言語対応

--- Gemfile
+++ Gemfile
@@ -86,3 +86,4 @@ end
 gem 'carrierwave'
 gem 'kaminari'
 gem 'devise'
+gem 'devise-i18n'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action do
+    I18n.locale = :ja
+  end
+
   protected
 
   def configure_permitted_parameters
