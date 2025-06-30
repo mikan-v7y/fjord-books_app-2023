1be8485f28a9b31244524093b7944c1236649ad8
mikan-v7y <mikan.v7y@gmail.com>
2025-06-30 14:31:56 +0900

    refactor: rubocopの指摘箇所を修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,7 +4,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
-
   # GET /resource/sign_up
   # def new
   #   super
@@ -68,6 +67,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:postal_code, :address, :self_introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[postal_code address self_introduction])
   end
 end
