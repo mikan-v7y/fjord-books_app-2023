a5fad5b4f3c6345684639b2750e4ecf88697cbd9
mikan-v7y <mikan.v7y@gmail.com>
2025-07-07 14:25:00 +0900

    feat: 投稿時間が日本時間で表示されるようにする

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,10 @@ module BooksApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.time_zone = 'Asia/Tokyo'
+
+    # デフォルトのロケールを日本（ja）に設定
+    config.i18n.default_locale = :ja
   end
 end
