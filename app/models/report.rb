5c04109861d765c6d69ee753452fdb27ec53de52
mikan-v7y <mikan.v7y@gmail.com>
2025-07-16 03:07:52 +0900

    feat: ReportとMentionの関連付けを行う

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,6 +4,12 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
+  has_many :given_mentions, class_name: 'Mention', foreign_key: 'source_report_id'
+  has_many :mentioning_reports, through: :given_mentions, source: :target_report
+
+  has_many :received_mentions, class_name 'Mention', foreign_key: 'target_report_id'
+  has_many: mentioned_reports, through: :received_mentions, source: :source_report
+
   validates :title, presence: true
   validates :content, presence: true
 
