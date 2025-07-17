4b810562238d1170c1dbb8e0471d0e48722a9ca3
mikan-v7y <mikan.v7y@gmail.com>
2025-07-17 15:42:49 +0900

    fix: コロンの位置を修正

--- app/models/report.rb
+++ app/models/report.rb
@@ -8,7 +8,7 @@ class Report < ApplicationRecord
   has_many :mentioning_reports, through: :given_mentions, source: :target_report
 
   has_many :received_mentions, class_name 'Mention', foreign_key: 'target_report_id', dependent: :destroy
-  has_many: mentioned_reports, through: :received_mentions, source: :source_report
+  has_many :mentioned_reports, through: :received_mentions, source: :source_report
 
   validates :title, presence: true
   validates :content, presence: true
