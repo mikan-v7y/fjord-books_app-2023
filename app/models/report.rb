867cca43927f8748b344254cccb2f20c0ed92b1d
mikan-v7y <mikan.v7y@gmail.com>
2025-07-17 15:09:07 +0900

    feat: Report削除時に関連するMentionレコードも自動削除

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,10 +4,10 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
-  has_many :given_mentions, class_name: 'Mention', foreign_key: 'source_report_id'
+  has_many :given_mentions, class_name: 'Mention', foreign_key: 'source_report_id', dependent: :destroy
   has_many :mentioning_reports, through: :given_mentions, source: :target_report
 
-  has_many :received_mentions, class_name 'Mention', foreign_key: 'target_report_id'
+  has_many :received_mentions, class_name 'Mention', foreign_key: 'target_report_id', dependent: :destroy
   has_many: mentioned_reports, through: :received_mentions, source: :source_report
 
   validates :title, presence: true
