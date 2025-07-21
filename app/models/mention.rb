dfb78513a3a754c036178558a79adfd2a81f73b1
mikan-v7y <mikan.v7y@gmail.com>
2025-07-21 16:22:35 +0900

    feat: DBに同じ言及関係が登録されないよう、モデルにバリデーションを追加

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -3,4 +3,6 @@
 class Mention < ApplicationRecord
   belongs_to :source_report, class_name: 'Report'
   belongs_to :target_report, class_name: 'Report'
+
+  validates :source_report_id, uniqueness: { scope: :target_report_id }
 end
