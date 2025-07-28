a63c33a8949422a8054b961166f2c0e00c82aae5
mikan-v7y <mikan.v7y@gmail.com>
2025-07-28 18:05:10 +0900

    refactor: コメントコントローラをbook用・report用に分割

---
+++ app/controllers/reports/comments_controller.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module Reports
+  class CommentsController < ApplicationController
+    before_action :set_report
+    before_action :set_comment, only: [:edit, :update, :destroy]
+    before_action :set_commentable
+
+    def create
+      @comment = @report.comments.build(comment_params)
+      @comment.user = current_user
+
+      if @comment.save
+        redirect_to @report, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
+      else
+        redirect_to @report, alert: t('controllers.comments.notice_create_failure', name: Comment.model_name.human)
+      end
+    end
+
+    def edit; end
+
+    def update
+      if @comment.update(comment_params)
+        redirect_to @report, notice: t('controllers.common.notice_update', name: Comment.model_name.human)
+      else
+        redirect_to @report, alert: t('controllers.common.notice_update_failure', name: Comment.model_name.human)
+      end
+    end
+
+    def destroy
+      @comment.destroy
+      redirect_to @report, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
+    end
+
+    private
+
+    def set_report
+      @report = Report.find(params[:report_id])
+    end
+
+    def set_comment
+      @comment = @report.comments.find(params[:id])
+    end
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
+
+    def set_commentable
+      @commentable = @report
+    end
+  end
+end
