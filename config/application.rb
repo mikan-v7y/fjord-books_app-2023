ce30e1c109fc7c42f8dd1765dd4781785d8985ec
mikan-v7y <mikan.v7y@gmail.com>
2025-07-29 14:32:40 +0900

    refactor: 不要な差分のため削除

--- config/application.rb
+++ config/application.rb
@@ -18,10 +18,5 @@ module BooksApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.time_zone = 'Asia/Tokyo'
-
-    # デフォルトのロケールを日本（ja）に設定
-    config.i18n.default_locale = :ja
   end
 end
