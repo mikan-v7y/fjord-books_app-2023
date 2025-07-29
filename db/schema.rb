81ae28c4fecf4a6495dc1cd0357046426ad5168f
mikan-v7y <mikan.v7y@gmail.com>
2025-07-29 14:56:43 +0900

    faet: コメントテーブルのcontentカラムにnot null制約を付与

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_07_07_101949) do
+ActiveRecord::Schema[7.0].define(version: 2025_07_29_053959) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -49,22 +49,32 @@ ActiveRecord::Schema[7.0].define(version: 2025_07_07_101949) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.text "content"
+    t.integer "user_id", null: false
     t.string "commentable_type", null: false
     t.integer "commentable_id", null: false
+    t.string "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "reports", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+  create_table "mentions", force: :cascade do |t|
+    t.integer "source_report_id", null: false
+    t.integer "target_report_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.index ["source_report_id", "target_report_id"], name: "index_mentions_on_source_report_id_and_target_report_id", unique: true
+    t.index ["source_report_id"], name: "index_mentions_on_source_report_id"
+    t.index ["target_report_id"], name: "index_mentions_on_target_report_id"
+  end
+
+  create_table "reports", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title", null: false
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
@@ -87,5 +97,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_07_07_101949) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "users"
+  add_foreign_key "mentions", "reports", column: "source_report_id"
+  add_foreign_key "mentions", "reports", column: "target_report_id"
   add_foreign_key "reports", "users"
 end
