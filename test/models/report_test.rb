6e5980c09a48d0a1970ecdf8f4075bb929d60d17
mikan-v7y <mikan.v7y@gmail.com>
2025-08-16 13:09:24 +0900

    test: save_mentionsで自己言及が保存されないことをテスト

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,6 +3,11 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
+  def setup
+    @alice_report = reports(:one)
+    @bob_report = reports(:two)
+  end
+
   test 'editable a Report' do
     report = reports(:one)
     assert report.editable?(users(:one))
@@ -17,5 +22,11 @@ class ReportTest < ActiveSupport::TestCase
     report = reports(:one)
     assert_instance_of Date, report.created_on
     assert_equal Date.new(2025, 8, 15), report.created_on
+  end
+
+  test 'save_mentions: not mention myself' do
+    @alice_report.content = "自分自身の日報を言及する http://localhost:3000/reports/#{@alice_report.id}"
+    @alice_report.send(:save_mentions)
+    assert_not_includes @alice_report.mentioning_reports, @alice_report
   end
 end
