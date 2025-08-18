945e3bd234b3e97c5bf05965c43a5a9c8fc49bcc
mikan-v7y <mikan.v7y@gmail.com>
2025-08-18 16:40:53 +0900

    test: 可読性向上のため、テスト名を変更

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -8,6 +8,8 @@ class ReportTest < ActiveSupport::TestCase
     @bob_report = reports(:two)
   end
 
+  # editable?をテスト
+
   test 'editable a Report' do
     report = reports(:one)
     assert report.editable?(users(:one))
@@ -18,13 +20,17 @@ class ReportTest < ActiveSupport::TestCase
     assert_not report.editable?(users(:one))
   end
 
+  # created_onをテスト
+
   test 'created_on' do
     report = reports(:one)
     assert_instance_of Date, report.created_on
     assert_equal Date.new(2025, 8, 15), report.created_on
   end
 
-  test 'save_mentions: self-reference is not registered' do
+  # save_mentionsをテスト
+
+  test 'self-reference is not registered' do
     @alice_report.content = "自分自身の日報を言及する http://localhost:3000/reports/#{@alice_report.id}"
     @alice_report.send(:save_mentions)
     assert_not_includes @alice_report.mentioning_reports, @alice_report
@@ -36,14 +42,14 @@ class ReportTest < ActiveSupport::TestCase
     assert_includes @alice_report.mentioning_reports, @bob_report
   end
 
-  test 'save_mentions: same mention relationship cannot be registered twice' do
+  test 'same mention relationship cannot be registered twice' do
     @alice_report.content = "aliceの日報でbobの日報を2回言及する。1回目の言及→http://localhost:3000/reports/#{@bob_report.id} 2回目の言及→http://localhost:3000/reports/#{@bob_report.id}"
     @alice_report.send(:save_mentions)
     assert_equal 1, @alice_report.mentioning_reports.size
     assert_not_equal 2, @alice_report.mentioning_reports.size
   end
 
-  test 'save:mentions: non-existent report is ignored' do
+  test 'non-existent report is ignored' do
     @alice_report.content = 'aliceの日報で存在しない日報を言及する http://localhost:3000/reports/7777'
     @alice_report.send(:save_mentions)
     assert_empty @alice_report.mentioning_reports
