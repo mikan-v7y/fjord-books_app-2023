01008a089dec4b262fb2c3dd283338ce3a829fec
mikan-v7y <mikan.v7y@gmail.com>
2025-08-30 19:24:19 +0900

    test: テストカバレッジ拡大のため、コメントのテストを追加

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,7 +3,11 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'alice can comment on bobs report' do
+    comment = comments(:alice_comment_on_bob_report)
+
+    assert_equal 'aliceがbobの日報にコメントを書きます', comment.content
+    assert_equal reports(:bob_report), comment.commentable
+    assert_equal users(:alice), comment.user
+  end
 end
