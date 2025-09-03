16ad6d9e5c725c85d3f21989de9d8b71d016e40d
mikan-v7y <mikan.v7y@gmail.com>
2025-09-03 21:02:17 +0900

    chore: 不要なファイルを削除

--- db/migrate/20250818113814_add_foreign_key_to_reports.rb
+++
@@ -1,6 +0,0 @@
-class AddForeignKeyToReports < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :reports, :user_id, :integer
-    add_reference :reports, :user, null: false, foreign_key: true
-  end
-end
